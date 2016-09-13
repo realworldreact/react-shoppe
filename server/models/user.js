fc952bb2bc36910f0294556b23c11d96a5920b69
Berkeley Martinez <Berkeley@robotie.com>
2016-09-13 14:31:27 -0700

    Feat(models): Add remove/add to cart user methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,160 @@
+import _ from 'lodash';
+
 module.exports = function(User) {
+  User.prototype.updateTo = function update$(updateData) {
+    const id = this.getId();
+    const updateOptions = { allowExtendedOperators: true };
+    if (
+        !updateData ||
+        typeof updateData !== 'object' ||
+        !Object.keys(updateData).length
+    ) {
+      return Promise.reject(new Error(
+        `
+          updateData must be an object with at least one key,
+          but got ${updateData} with ${Object.keys(updateData).length}
+        `.split('\n').join(' ')
+      ));
+    }
+    console.log('updating user: ', id);
+    console.log('update data: ', updateData);
+    return this.constructor.updateAll({ id }, updateData, updateOptions);
+  };
+
+  User.prototype.addToCart = function addToCart(itemId, count = 1) {
+    const user = this;
+    const { Product } = User.app.models;
+    // validate item is actual product
+    return Product.findById(itemId)
+      .then(product => {
+        if (!product) {
+          throw new Error(`no product found with id ${itemId}.`);
+        }
+        return product;
+      })
+      .then(product => {
+        console.log('item: ', product._id);
+        const newItems = [ ...user.cart.items ];
+        const index = _.findIndex(newItems, item => item.id === product.id);
+        if (index !== -1) {
+          const oldItem = newItems[index];
+          newItems[index] = { ...oldItem, count: oldItem.count + count };
+        } else {
+          newItems.push({
+            id: product.getId(),
+            count
+          });
+        }
+        const updateData = {
+          cart: {
+            ...user.cart,
+            items: newItems
+          }
+        };
+        return user.updateTo(updateData)
+          .then(() => {
+            return updateData;
+          });
+      });
+  };
+
+  User.prototype.removeFromCart = function removeFromCart(itemId, count = 1) {
+    const user = this;
+    const { Product } = User.app.models;
+    // validate item is actual product
+    return Product.findById(itemId)
+      .then(product => {
+        if (!product) {
+          throw new Error(`no product found with id ${itemId}.`);
+        }
+        return product;
+      })
+      .then(product => {
+        console.log('item: ', product._id);
+        const newItems = [ ...user.cart.items ];
+        const index = _.findIndex(newItems, item => item.id === product.id);
+        if (index !== -1) {
+          const oldItem = newItems[index];
+          newItems[index] = {
+            ...oldItem,
+            count: Math.max(oldItem.count - count, 0)
+          };
+        } else {
+          newItems.push({
+            id: product.getId(),
+            count
+          });
+        }
+        const updateData = {
+          cart: {
+            ...user.cart,
+            items: newItems
+          }
+        };
+        return user.updateTo(updateData)
+          .then(() => {
+            return updateData;
+          });
+      });
+  };
+
+  User.remoteMethod(
+    'addToCart',
+    {
+      isStatic: false,
+      description: 'updates the users cart with item',
+      accepts: [
+        {
+          arg: 'productId',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'count',
+          type: 'number',
+          required: false
+        }
+      ],
+      returns: [
+        {
+          arg: 'status',
+          type: 'object'
+        }
+      ],
+      http: {
+        path: '/add-to-cart',
+        verb: 'POST'
+      }
+    }
+  );
+
+  User.remoteMethod(
+    'removeFromCart',
+    {
+      isStatic: false,
+      description: 'removes the item from users cart',
+      accepts: [
+        {
+          arg: 'productId',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'count',
+          type: 'number',
+          required: false
+        }
+      ],
+      returns: [
+        {
+          arg: 'status',
+          type: 'object'
+        }
+      ],
+      http: {
+        path: '/remove-from-cart',
+        verb: 'POST'
+      }
+    }
+  );
 };
