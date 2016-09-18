de4370b2245426c067dc460bcad04dfe90c235f6
Berkeley Martinez <Berkeley@robotie.com>
2016-09-18 11:14:52 -0700

    Feat(utils): Add form serializer

---
+++ common/utils/serialize-form.js
@@ -0,0 +1,9 @@
+export function serializeForm(form) {
+  const data = [].filter.call(form.elements, node => !!node.name)
+    .reduce((data, node) => {
+      data[node.name] = node.value;
+      return data;
+    }, {});
+
+  return JSON.stringify(data);
+}
