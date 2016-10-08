f72f961e4aa6fb0a82ce5e1923c07fe947fc177a
Berkeley Martinez <Berkeley@robotie.com>
2016-10-08 14:11:02 -0700

    Feat: auto-fetch user

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,41 @@
 import React, { cloneElement, PropTypes, Component } from 'react';
+
 import Nav from './Nav.jsx';
+import { fetchUser } from '../api.js';
 
 export default class App extends Component {
   constructor(...props) {
     super(...props);
     this.state = {
-      user: null
+      user: {}
     };
     this.addUser = this.addUser.bind(this);
   }
 
+  componentDidMount() {
+    if (localStorage.userId && localStorage.accessToken) {
+      const userId = localStorage.getItem('userId');
+      const accessToken = localStorage.getItem('accessToken');
+      fetchUser(userId, accessToken)
+        .then(user => this.addUser(user));
+    }
+  }
+
   addUser(user = {}) {
+    if (user.id && user.accessToken) {
+      localStorage.setItem('userId', user.id);
+      localStorage.setItem('accessToken', user.accessToken);
+    }
     this.setState({ user });
   }
 
   render() {
+    const { username } = this.state.user;
     return (
       <div className='app'>
-        <Nav />
+        <Nav
+          name={ username }
+        />
         <div className='app-child'>
           {
             cloneElement(
