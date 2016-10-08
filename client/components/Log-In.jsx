0417904827b12525d429667fc33e65310931e352
Berkeley Martinez <Berkeley@robotie.com>
2016-10-08 14:16:35 -0700

    Feat: log-in

--- client/components/Log-In.jsx
+++ client/components/Log-In.jsx
@@ -1,13 +1,52 @@
 import React, { PropTypes } from 'react';
+import { browserHistory } from 'react-router';
+import { logIn } from '../api.js';
 
 const propTypes = {
   addUser: PropTypes.func
 };
 
 export default class LogIn extends React.Component {
+  constructor(...props) {
+    super(...props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    logIn(form)
+      .then(user => this.props.addUser(user))
+      .then(() => browserHistory.push('/'));
+  }
+
   render() {
     return (
-      <div className='auth-login' />
+      <div className='auth-login'>
+        <form
+          onSubmit={ this.handleSubmit }
+          >
+          <label>
+            <input
+              name='email'
+              placeholder='email'
+              type='email'
+            />
+          </label>
+          <label>
+            <input
+              name='password'
+              placeholder='password'
+              type='password'
+            />
+          </label>
+          <button
+            type='submit'
+            >
+            Submit
+          </button>
+        </form>
+      </div>
     );
   }
 }
