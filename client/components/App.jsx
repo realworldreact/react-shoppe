ee49f47caa6d94894c8f0ce01c680b478dd0c2db
Berkeley Martinez <Berkeley@robotie.com>
2016-10-08 12:26:13 -0700

    Feat: Signup form and save user state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,32 @@
-import React, { PropTypes, Component } from 'react';
+import React, { cloneElement, PropTypes, Component } from 'react';
 import Nav from './Nav.jsx';
 
 export default class App extends Component {
+  constructor(...props) {
+    super(...props);
+    this.state = {
+      user: null
+    };
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(user = {}) {
+    this.setState({ user });
+  }
+
   render() {
     return (
       <div className='app'>
         <Nav />
         <div className='app-child'>
-          { this.props.children }
+          {
+            cloneElement(
+              this.props.children,
+              {
+                addUser: this.addUser
+              }
+            )
+          }
         </div>
       </div>
     );
--- client/components/Sign-Up.jsx
+++ client/components/Sign-Up.jsx
@@ -1,13 +1,56 @@
 import React, { PropTypes } from 'react';
+import { signUp } from '../api.js';
 
 const propTypes = {
   addUser: PropTypes.func
 };
 
 export default class SignUp extends React.Component {
+  constructor(...props) {
+    super(...props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    signUp(form).then(user => this.props.addUser(user));
+  }
+
   render() {
     return (
-      <div className='auth-signup' />
+      <div className='auth-signup'>
+        <form
+          onSubmit={ this.handleSubmit }
+          >
+          <label>
+            <input
+              name='username'
+              placeholder='name'
+              type='text'
+            />
+          </label>
+          <label>
+            <input
+              name='email'
+              placeholder='email'
+              type='email'
+            />
+          </label>
+          <label>
+            <input
+              name='password'
+              placeholder='password'
+              type='password'
+            />
+          </label>
+          <button
+            type='submit'
+            >
+            Submit
+          </button>
+        </form>
+      </div>
     );
   }
 }
