0bb4be107df99ab728c4639d35238cf24d6e788a
Berkeley Martinez <Berkeley@robotie.com>
2016-11-20 10:45:50 -0800

    feat: Add redux to react

--- client/index.js
+++ client/index.js
@@ -1,10 +1,35 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory as history } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import routes from './routes.jsx';
 
+const devToolsExt = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__() ||
+  (x => x);
+
+function reducer(state = { count: 1 }, action) {
+  if (action.type === 'INC') {
+    state.count = state.count + 1;
+    return state;
+  }
+  return state;
+}
+
+const store = createStore(reducer, devToolsExt);
+
+// <Provider store={ store }>
+//   <Router routes={ routes } history={ history } />
+// </Provider>
 render(
-  createElement(Router, { routes, history }),
+  createElement(
+    Provider,
+    { store },
+    createElement(Router, { routes, history }),
+  ),
   window.document.getElementById('app')
 );
+
+// reducer(CurrentState, Action) => newState
