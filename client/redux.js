d5afe86ec64d5bbb979bd6cc1dafee6a202d3d73
Berkeley Martinez <Berkeley@robotie.com>
2017-03-08 12:30:38 -0800

    feat(redux): add types and action creatores
    add types and action creatores

--- client/redux.js
+++ client/redux.js
@@ -1,13 +1,23 @@
+const types = {
+  INC: 'INC',
+  DEC: 'DEC'
+};
 const initialState = {
   count: 0
 };
+
+export const increment = () => {
+  return {
+    type: types.INC
+  };
+};
 export default (state = initialState, action) => {
-  if (action.type === 'INC') {
+  if (action.type === types.INC) {
     return Object.assign({}, state, {
       count: state.count + 1
     });
   }
-  if (action.type === 'DEC') {
+  if (action.type === types.DEC) {
     return {
       ...state,
       count: state.count - 1
