56a6a3fb9aa1f93e9ebac35eb378008288d498db
Berkeley Martinez <Berkeley@robotie.com>
2017-03-08 15:18:46 -0800

    feat(app): add redux-thunk middleware

--- client/index.js
+++ client/index.js
@@ -1,15 +1,25 @@
 import { createElement } from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 import { render } from 'react-dom';
 import { Router, browserHistory as history } from 'react-router';
 
 import routes from './routes.jsx';
 import reducer from './redux.js';
 
+const middlewareStoreEnhancer = applyMiddleware(
+  thunkMiddleware
+);
+
+const devToolStoreEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__() ||
+  ((x) => x);
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(middlewareStoreEnhancer, devToolStoreEnhancer)
 );
 // <Provider store={ store }>
 //   <Router routes={ routes} />
