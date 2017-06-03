736fd9e48daddf47fc35cde154cdf7275497938e
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-06-03 13:21:10 -0500

    feat(app): render first component

--- client/index.js
+++ client/index.js
@@ -1,3 +1,20 @@
+import { createElement } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from '../common/App.jsx';
+
 const win = typeof window !== 'undefined' ? window : {};
 const container = win.document.getElementById('app');
-container.innerHTML = 'Happy Coding';
+
+// <Router>
+//  <App />
+// </Router>
+render(
+  createElement(
+    Router,
+    null,
+    createElement(App)
+  ),
+  container
+);
