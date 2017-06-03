062f0c05d5c3d20b9dcc41e01c148e4992a0c371
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-06-03 15:28:38 -0500

    feat(products): Add controlled products search

--- common/Products/Products.jsx
+++ common/Products/Products.jsx
@@ -7,15 +7,33 @@ const propTypes = {
 };
 
 export default class Products extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      search: ''
+    };
+  }
+
   render() {
+    const { search } = this.state;
     const { products } = this.props;
     return (
       <div className='products'>
         <div className='products-search'>
-          <input className='products-search_input' />
+          <input
+            className='products-search_input'
+            onChange={ (e) => {
+              console.log('searching for ', e.target.value);
+              this.setState({
+                search: e.target.value
+              });
+            } }
+            value={ search }
+          />
         </div>
         <div className='products-lists'>
-          { products.map(({ name, description, image }) => {
+          { products
+              .map(({ name, description, image }) => {
             return (
               <Product
                 description={ description }
